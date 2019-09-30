582ac4e7942bd9abee1c58b0794e2c5961ad8e4e
Nikita <Nikita.Titov@waveaccess.ru>
2019-09-30 18:26:13 +0300

    - Mongo

--- src/main/java/tver/wa/repositories/user/UserRepository.java
+++ src/main/java/tver/wa/repositories/user/UserRepository.java
@@ -1,16 +1,12 @@
 package tver.wa.repositories.user;
 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import tver.wa.model.secret.santa.User;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends ReactiveMongoRepository<User, UUID> {
 
-    Flux<User> findAll();
-
-    Mono<User> findById(UUID uuid);
 }
