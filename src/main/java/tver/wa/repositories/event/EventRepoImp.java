743b4f7b3a4ed844c9ef9083275c4cb7446072e3
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2019-10-01 12:57:35 +0300

    EventRepository changed;

--- src/main/java/tver/wa/repositories/event/EventRepoImp.java
+++
@@ -1,7 +0,0 @@
-package tver.wa.repositories.event;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class EventRepoImp implements EventRepository {
-}
--- src/main/java/tver/wa/repositories/event/EventRepository.java
+++ src/main/java/tver/wa/repositories/event/EventRepository.java
@@ -1,7 +1,11 @@
 package tver.wa.repositories.event;
 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import tver.wa.model.secret.santa.Event;
+
+import java.util.UUID;
 
 @Repository
-public interface EventRepository {
+public interface EventRepository extends ReactiveMongoRepository<Event, UUID> {
 }
