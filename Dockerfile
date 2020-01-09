edbd51618bc836bca82bae837cc55e54b0a66945
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2020-01-09 13:51:09 +0300

    refactoring: - config-server added - santa-api added - env file added;

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM alpine:3.9
-
-RUN echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/main' >> /etc/apk/repositories
-RUN echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/community' >> /etc/apk/repositories
-RUN apk update
-RUN apk add mongodb=4.0.5-r0
-
-EXPOSE 27017
--- santa-api/src/test/java/tver/wa/services/user/UserServiceImpIntegrationTest.java
+++ santa-api/src/test/java/tver/wa/services/user/UserServiceImpIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -19,12 +18,6 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringRunner.class) // Junit4 runner
 @DataMongoTest // Mongo embedded auto configuration
 @Import({UserServiceImp.class}) // enable bean injection for beans in {}
-@ActiveProfiles("integration") // active profiler
-// the same configuration:
-//@RunWith(SpringRunner.class) // Junit4 runner
-//@SpringBootTest // test bean injection
-//@AutoConfigureDataMongo  // Mongo embedded auto configuration
-//@ActiveProfiles("integration") // active profiler
 public class UserServiceImpIntegrationTest {
 
     @Autowired
